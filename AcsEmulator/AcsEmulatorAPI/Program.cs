40011a5b6cde65d1ea95ff142aa629f5b099ba00
Mikhail Bakalov <mbakalov@microsoft.com>
2022-09-20 12:57:13 -0700

    Add SQLite scaffold and empty Users table

--- AcsEmulator/AcsEmulatorAPI/Program.cs
+++ AcsEmulator/AcsEmulatorAPI/Program.cs
@@ -1,4 +1,11 @@
+using AcsEmulatorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("EmulatorDb");
+builder.Services.AddDbContext<AcsDbContext>(options =>
+	options.UseSqlite(connectionString));
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,6 +19,15 @@ if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
 	app.UseSwaggerUI();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<AcsDbContext>();
+    context.Database.EnsureCreated();
+	// TODO: seed data if we want
 }
 
 app.UseHttpsRedirection();
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # acs-emulator
 Local emulator to run Azure Communication Services client SDKs without a provisioned ACS resource
+
+## Getting started
+
+* Install `dotnet ef` tool. Install by doing `dotnet tool install --global dotnet-ef` [Link for details](https://docs.microsoft.com/en-us/ef/core/cli/dotnet)
+
+* Run `dotnet ef database update` to update your local copy of the `AcsEmulator.db` in case new migrations were added.
