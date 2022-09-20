6d7e413f5601653e0e29d92a0ce22dabda1f76b3
Mikhail Bakalov <mbakalov@microsoft.com>
2022-09-20 13:25:58 -0700

    Rename Id to RawId

--- AcsEmulator/AcsEmulatorAPI/Models/User.cs
+++ AcsEmulator/AcsEmulatorAPI/Models/User.cs
@@ -1,9 +1,14 @@
 ﻿#nullable disable
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AcsEmulatorAPI.Models
 {
     public class User
     {
-        public string Id { get; set; }
+        [Column("Id")]
+        [Key]
+        public string RawId { get; set; }
     }
 }
