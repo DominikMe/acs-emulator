18363f7e063983335b69d83525971b322f4060ce
Mikhail Bakalov <mbakalov@microsoft.com>
2022-09-22 09:46:53 -0700

    Refactor controllers
    Separate chat and chatThread.
    Attempt to "extract method" for getting context and basic validation.

---
+++ AcsEmulator/AcsEmulatorAPI/ChatController.cs
@@ -0,0 +1,73 @@
+﻿using AcsEmulatorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace AcsEmulatorAPI
+{
+	// https://github.com/Azure/azure-rest-api-specs/blob/main/specification/communication/data-plane/Chat/stable/2021-09-07/communicationserviceschat.json
+	public static class ChatController
+	{
+		public static void AddChatEndpoints(this WebApplication app)
+		{
+			app.MapPost("/chat/threads", [Authorize] async (ClaimsPrincipal principal, AcsDbContext db, CreateChatThreadRequest req) =>
+			{
+				string userRawId = principal.Claims.First(x => x.Type == "skypeid").Value;
+
+				var user = await db.Users.FindAsync(userRawId);
+
+				if (user == null)
+				{
+					return Results.Forbid();
+				}
+
+				var t = ChatThread.CreateNew(req.Topic, user);
+
+				var participants = req.Participants ?? new List<ChatParticipant>();
+
+				// the chat thread creator always gets added as a participant
+				if (!participants.Any(x => x.CommunicationIdentifier.RawId == userRawId))
+				{
+					// todo: handle displayName and shareHistoryTime defaults - just null or default values in db?
+					participants.Add(new ChatParticipant(new CommunicationIdentifier(userRawId), null, null));
+				}
+
+				await t.AddParticipants(db, participants);
+
+				await db.ChatThreads.AddAsync(t);
+				await db.SaveChangesAsync();
+
+				var result = new
+				{
+					ChatThread = new
+					{
+						t.Id,
+						t.Topic,
+						t.CreatedOn,
+						CreatedByCommunicationIdentifier = new
+						{
+							user.RawId
+						}
+					}
+				};
+
+				return Results.Created($"/chat/threads/{t.Id}", result);
+			});
+
+			app.MapGet("/chat/threads", [Authorize] async (ClaimsPrincipal principal, AcsDbContext db) =>
+			{
+				string userRawId = principal.Claims.First(x => x.Type == "skypeid").Value;
+
+				var threads = await db.ChatThreads
+					.Include(t => t.Participants.Where(p => p.RawId == userRawId))
+					.Select(t => new { t.Id, t.Topic })
+					.ToListAsync();
+
+				return Results.Ok(new
+				{
+					value = threads
+				});
+			});
+		}
+	}
+}
---
+++ AcsEmulator/AcsEmulatorAPI/ChatThreadController.cs
@@ -0,0 +1,225 @@
+﻿using AcsEmulatorAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AcsEmulatorAPI
+{
+	public static class ChatThreadController
+	{
+		record ThreadRequestContext;
+
+		record ThreadRequestErrorContext(IResult ErrorResult) : ThreadRequestContext;
+		record ThreadRequestValidContext(User ThisUser, ChatThread ThisThread): ThreadRequestContext;
+
+		public static void AddChatThreadEndpoints(this WebApplication app)
+		{
+			app.MapPost(
+				"/chat/threads/{chatThreadId}/participants/:add",
+				[Authorize] async (ClaimsPrincipal principal, AcsDbContext db, string chatThreadId, AddChatParticipantsRequest req) =>
+				{
+					var context = await GetRequestContext(
+						principal,
+						async (userId) =>
+						{
+							return await db.Users
+								.Include(u => u.Threads)
+								.Include(u => u.UserChatThreads)
+								.FirstOrDefaultAsync(u => u.RawId == userId);
+						},
+						async () =>
+						{
+							return await db.ChatThreads
+								.Include(t => t.Participants)
+								.Include(t => t.UserChatThreads)
+								.FirstOrDefaultAsync(t => t.Id == chatThreadId);
+						});
+
+					if (context is ThreadRequestValidContext (_, ChatThread thisThread))
+					{
+						await thisThread.AddParticipants(db, req.Participants);
+
+						await db.SaveChangesAsync();
+
+						// TODO: why does Swagger say it should return 201?
+						return Results.Created($"/chat/threads/{chatThreadId}/participants", new { });
+					}
+					else if (context is ThreadRequestErrorContext err)
+					{
+						return err.ErrorResult;
+					}
+					else
+					{
+						throw new Exception($"Unexpected context type: {context.GetType()}");
+					}
+				});
+
+			app.MapGet(
+				"/chat/threads/{chatThreadId}/participants",
+				[Authorize] async (ClaimsPrincipal principal, AcsDbContext db, string chatThreadId) =>
+				{
+					var context = await GetRequestContext(
+						principal,
+						async (userId) => await db.Users.FindAsync(userId),
+						async () =>
+						{
+							return await db.ChatThreads
+								.Include(t => t.Participants)
+								.Include(t => t.UserChatThreads)
+								.FirstOrDefaultAsync(t => t.Id == chatThreadId);
+						});
+
+					if (context is ThreadRequestValidContext(_, ChatThread thisThread))
+					{
+						var participants = thisThread.UserChatThreads.Select(uct => new
+						{
+							communicationUserIdentifier = uct.UserId,
+							uct.DisplayName,
+							uct.ShareHistoryTime
+						});
+
+						// TODO: paging
+						return Results.Ok(new { value = participants });
+					}
+					else if (context is ThreadRequestErrorContext err)
+					{
+						return err.ErrorResult;
+					}
+					else
+					{
+						throw new Exception($"Unexpected context type: {context.GetType()}");
+					}
+				});
+
+			app.MapPost(
+				"/chat/threads/{chatThreadId}/messages",
+				[Authorize] async (ClaimsPrincipal principal, AcsDbContext db, string chatThreadId, SendChatMessageRequest req) =>
+				{
+					var context = await GetRequestContext(
+						principal,
+						async (userId) => await db.Users.FindAsync(userId),
+						async () =>
+						{
+							return await db.ChatThreads
+								.Include(t => t.Participants)
+								.Include(t => t.UserChatThreads)
+								.Include(t => t.Messages)
+								.FirstOrDefaultAsync(t => t.Id == chatThreadId);
+						});
+
+					if (context is ThreadRequestValidContext(User thisUser, ChatThread thisThread))
+					{
+						var msg = new ChatMessage
+						{
+							Content = req.Content,
+							Sender = thisUser,
+							SenderDisplayName = req.SenderDisplayName,
+							Type = req.Type ?? ChatMessageType.Text
+						};
+						thisThread.Messages.Add(msg);
+
+						await db.SaveChangesAsync();
+
+						return Results.Created(
+							$"/chat/threads/{chatThreadId}/messages/{msg.Id}",
+							new { msg.Id });
+					}
+					else if (context is ThreadRequestErrorContext err)
+					{
+						return err.ErrorResult;
+					}
+					else
+					{
+						throw new Exception($"Unexpected context type: {context.GetType()}");
+					}
+				});
+
+			app.MapGet(
+				"/chat/threads/{chatThreadId}/messages",
+				[Authorize] async (ClaimsPrincipal principal, AcsDbContext db, string chatThreadId) =>
+				{
+					var context = await GetRequestContext(
+						principal,
+						async (userId) => await db.Users.FindAsync(userId),
+						async () =>
+						{
+							return await db.ChatThreads
+								.Include(t => t.Participants)
+								.Include(t => t.Messages)
+								.FirstOrDefaultAsync(t => t.Id == chatThreadId);
+						});
+
+					if (context is ThreadRequestValidContext(_, ChatThread thisThread))
+					{
+						var messages = thisThread.Messages
+							.Select(m => new
+							{
+								m.Id,
+
+								// TODO: convert enum properly
+								type = "text",
+
+								// TODO
+								sequenceId = "1",
+
+								// TODO
+								versionId = "1",
+
+								// TODO: support other message types (topic updated, participants updated)
+								content = new
+								{
+									message = m.Content
+								},
+
+								m.SenderDisplayName,
+
+								// TODO: save with message
+								createdOn = DateTimeOffset.UtcNow,
+
+								senderCommunicationIdentifier = new
+								{
+									rawId = m.Sender.RawId
+								}
+							});
+
+						return Results.Ok(messages);
+					}
+					else if (context is ThreadRequestErrorContext err)
+					{
+						return err.ErrorResult;
+					}
+					else
+					{
+						throw new Exception($"Unexpected context type: {context.GetType()}");
+					}
+				});
+		}
+
+		private static async Task<ThreadRequestContext> GetRequestContext(
+			ClaimsPrincipal principal,
+			Func<string, Task<User?>> getUser,
+			Func<Task<ChatThread?>> getThread)
+		{
+			string userRawId = principal.Claims.First(x => x.Type == "skypeid").Value;
+			
+			var thisUser = await getUser(userRawId);
+			var thisThread = await getThread();
+
+			if (thisThread == null)
+			{
+				return new ThreadRequestErrorContext(Results.NotFound());
+			}
+			else if (thisUser == null)
+			{
+				return new ThreadRequestErrorContext(Results.Forbid());
+			}
+			else if (!thisThread.Participants.Any(p => p.RawId == userRawId)) // Current user is not a participant in the requested thread
+			{
+				return new ThreadRequestErrorContext(Results.Forbid());
+			}
+
+			return new ThreadRequestValidContext(thisUser, thisThread);
+		}
+	}
+}
