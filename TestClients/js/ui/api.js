65ee49d2ee764ec1aec65dee2a7a4861ffae1987
Dominik Messinger <domessin@microsoft.com>
2022-09-22 11:59:06 -0700

    fetch token when selecting a user, progressively display next steps

--- TestClients/js/ui/api.js
+++ TestClients/js/ui/api.js
@@ -12,6 +12,20 @@ async function createUser()
     return await response.json();
 }
 
+async function getUserToken(identity)
+{
+    const response = await fetch(`https://localhost/identities/${identity}/:issueAccessToken`, {
+        method: "post",
+        body: JSON.stringify({
+            scopes: ["chat"]
+        }),
+        headers: {
+            "content-type": "application/json"
+        }
+    });
+    return await response.json();
+}
+
 async function getUsers()
 {
     const response = await fetch("https://localhost/identities");
