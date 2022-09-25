fc4674f91aaf56a5894f52ec745c89452d24a598
Mikhail Bakalov <mbakalov@microsoft.com>
2022-09-25 13:48:51 -0700

    Add basic info to the quickstart

--- AcsEmulator/acs-emulator-ui/src/components/Quickstart.tsx
+++ AcsEmulator/acs-emulator-ui/src/components/Quickstart.tsx
@@ -1,7 +1,18 @@
-import { Text } from '@fluentui/react';
+import { Stack, IStackTokens, Text, TextField, Link } from '@fluentui/react';
 
 export const Quickstart = () => {
+
+  const stackTokens: IStackTokens ={
+    childrenGap: 20,
+    padding: 20
+  }
+
   return (
-    <Text>This is a quickstart</Text>
+    <Stack tokens={stackTokens}>
+      <Text variant='large'>Congratulations! Your Azure Communication Services Emulator is running.</Text>
+      <Text>Connect a sample app to it, or browse the <Link href='https://localhost/swagger/index.html'>Swagger API definition.</Link></Text>
+      <TextField label='Endpoint' readOnly defaultValue='https://localhost'/>
+      <TextField label='Connection String' readOnly defaultValue='endpoint=https://localhost/;accessKey=pw=='/>
+    </Stack>
   );
 }
