3d6f185310af9b6a3b44846aba8c397b773a797a
Mikhail Bakalov <46465618+mbakalov@users.noreply.github.com>
2023-02-17 16:26:31 -0800

    Add sms test client samples (#44)
    * Copy over sms sdk samples
    
    * Fix sendWithOptions.
    
    Use SmsSendOptions from the npm packagem not sdk source code which
    happens to be co-located with the sample
    
    * Add stub webhook listener
    
    * Add listener for actual EventGrid events
    
    * BUG: send 200 response from webhook

---
+++ TestClients/js/sms/src/listenSmsEvents.ts
@@ -0,0 +1,55 @@
+import express from "express";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+  // console.log('Got callback. Body');
+  // console.log(req.body);
+
+  const validationEventType = "Microsoft.EventGrid.SubscriptionValidationEvent";
+  const deliveryReportEventType = "Microsoft.Communication.SMSDeliveryReportReceived";
+  const smsReceivedEventType = "Microsoft.Communication.SMSReceived";
+
+  for (var events in req.body) {
+    var body = req.body[events];
+    // Deserialize the event data into the appropriate type based on event type
+    if (body.data && body.eventType == validationEventType) {
+        console.log("Got SubscriptionValidation event data, validation code: " + body.data.validationCode + " topic: " + body.topic);
+
+        // Do any additional validation (as required) and then return back the below response
+        var code = body.data.validationCode;
+        res.status(200).json({ "ValidationResponse": code });
+    }
+    else if (body.data && body.eventType == deliveryReportEventType) {
+      console.log("Got delivery reoport event data, report details:");
+      console.log({
+        from: body.data.from,
+        to: body.data.to,
+        deliveryStatus: body.data.deliveryStatus,
+        deliveryStatusDetails: body.data.deliveryStatusDetails,
+        tag: body.data.tag
+      });
+
+      res.status(200).send();
+    }
+    else if (body.data && body.eventType == smsReceivedEventType) {
+      console.log("Got incoming SMS message, details:");
+      console.log({
+        from: body.data.from,
+        to: body.data.to,
+        message: body.data.message
+      });
+
+      res.status(200).send();
+    }
+  }
+});
+
+const port = process.env.LISTEN_PORT || "8080";
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
