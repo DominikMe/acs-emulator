8c530f7cd8cbadba1be30f1665959ede0a2cbbe8
Dominik Messinger <domessin@microsoft.com>
2023-05-09 16:47:21 +0200

    bundle web ui. Serving prod build still has issues

--- AcsEmulator/AcsEmulatorCLI/Program.cs
+++ AcsEmulator/AcsEmulatorCLI/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.CommandLine;
-using System.CommandLine.Invocation;
 using System.Diagnostics;
 
 class Program
@@ -11,20 +10,21 @@ class Program
 		Command run = new("run", description: "Run the emulator.");
 		Command openApi = new("openApi", description: "Open the emulator's API in Swagger UI.");
 		Command openDB = new("openDB", description: "Open the emulator's sqlite database.");
-		Command openExplorer = new("openExplorer", description: "Open the emulator data explorer.");
+		Command openUI = new("openUI", description: "Open the emulator UI.");
 		Command clean = new("clean", description: "Clean all data and reset the emulator state.");
 		Command connectionString = new("connectionString", description: "Get the ACS connection string for the emulator.");
 		Command installCert = new("installCert", description: "Install root cert to reroute real-time channel.");
 		Command uninstallCert = new("uninstallCert", description: "Uninstall root cert.");
 
-		run.SetHandler(RunEmulator);
+		run.SetHandler(Run);
 		openApi.SetHandler(OpenSwaggerUI);
 		openDB.SetHandler(OpenDB);
+		openUI.SetHandler(OpenUI);
 
 		rootCommand.AddCommand(run);
 		rootCommand.AddCommand(openApi);
 		rootCommand.AddCommand(openDB);
-		rootCommand.AddCommand(openExplorer);
+		rootCommand.AddCommand(openUI);
 		rootCommand.AddCommand(clean);
 		rootCommand.AddCommand(connectionString);
 		rootCommand.AddCommand(installCert);
@@ -33,17 +33,26 @@ class Program
 		return await rootCommand.InvokeAsync(args);
 	}
 
-	private static void RunEmulator()
+	private static async Task Run()
+	{
+		_ = StartEmulator();
+		await OpenUI();
+	}
+
+	private static Task StartEmulator()
 	{
 		using Process proc = new();
 		proc.StartInfo.WorkingDirectory = AppContext.BaseDirectory;
 		proc.StartInfo.FileName = "dotnet";
-		proc.StartInfo.Arguments = "--additional-deps AcsEmulatorCLI.deps.json AcsEmulatorAPI.dll";
+		proc.StartInfo.Arguments = "--additional-deps AcsEmulatorCLI.deps.json AcsEmulatorAPI.dll --urls=https://localhost/";
 		proc.StartInfo.UseShellExecute = true;
 		proc.Start();
+		return proc.WaitForExitAsync();
 	}
 
-	private static void OpenSwaggerUI() => Process.Start(new ProcessStartInfo("http://localhost:5000/swagger") { UseShellExecute = true });
+	private static void OpenSwaggerUI() => Process.Start(new ProcessStartInfo("https://localhost/swagger") { UseShellExecute = true });
+
+	
 	private static void OpenDB()
 	{
 		try
@@ -56,6 +65,35 @@ class Program
 				Console.WriteLine("Please first run 'acs-emulator run' to create the database.");
 			else
 				Console.WriteLine(ex.Message);
+		}
+	}
+
+	private static async Task OpenUI()
+	{
+		try
+		{
+			using Process install = new();
+			install.StartInfo.WorkingDirectory = AppContext.BaseDirectory;
+			install.StartInfo.FileName = "npm.cmd";
+			install.StartInfo.Arguments = "install -g serve";
+			install.StartInfo.UseShellExecute = false;
+			install.Start();
+			install.WaitForExit();
+
+			using Process serve = new();
+			serve.StartInfo.WorkingDirectory = AppContext.BaseDirectory;
+			serve.StartInfo.FileName = "serve";
+			serve.StartInfo.Arguments = "-s .";
+			serve.StartInfo.UseShellExecute = true;
+			serve.Start();
+
+			await Task.Delay(3000);
+
+			Process.Start(new ProcessStartInfo("http://localhost:3000") { UseShellExecute = true });
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
 		}
 	}
 }
