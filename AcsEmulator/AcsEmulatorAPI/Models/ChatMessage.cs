b8776cbb68193b96adbded0e073e147b1a93036d
Vincent Gerard <vgerard@microsoft.com>
2024-03-12 16:09:06 -0600

    Change Chat DB Schema to match JSON payload/protocol

--- AcsEmulator/AcsEmulatorAPI/Models/ChatMessage.cs
+++ AcsEmulator/AcsEmulatorAPI/Models/ChatMessage.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace AcsEmulatorAPI.Models
 {
-	//[Table("ChatMessages")]
+	[Table("ChatMessages")]
 	public class ChatMessage
 	{
 		public Guid Id { get; set; }
@@ -17,10 +18,13 @@ namespace AcsEmulatorAPI.Models
 
 		public DateTimeOffset CreatedOn { get; set; } = DateTimeOffset.UtcNow;
 
-		public int SequenceId { get; set; }
+		// SequenceId is sent as String in (/chat/threads/{thread_id}/messages)
+        public string SequenceId { get; set; }
+
+		public string? VersionId { get; set; }
 	}
 
-	//[Table("AddParticipantsChatMessages")]
+	[Table("AddParticipantsChatMessages")]
 	public class AddParticipantsChatMessage: ChatMessage
 	{
 		public virtual ICollection<AddedParticipant> AddedParticipants { get; set; }
@@ -36,4 +40,10 @@ namespace AcsEmulatorAPI.Models
 
 		public DateTimeOffset? ShareHistoryTime { get; set; }
 	}
+
+
+	// ChatMessage in Payload do not match above record ( SequenceId is string, and many other fields are missing)
+	// Creating another one, but will need to be merged
+    // Response of /chat/threads/{thread_id}/messages GET
+	public record ChatMessagesResponse(List<ChatMessage> Value);
 }
