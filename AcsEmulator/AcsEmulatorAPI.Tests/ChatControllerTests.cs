c812026f96b995b6275ebe6e96db7056e34a1837
Dominik Messinger <domessin@microsoft.com>
2024-03-13 17:54:54 +0100

    support PhoneNumber identifier, refactor tests to not use blocking Result calls

--- AcsEmulator/AcsEmulatorAPI.Tests/ChatControllerTests.cs
+++ AcsEmulator/AcsEmulatorAPI.Tests/ChatControllerTests.cs
@@ -1,11 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using AcsEmulatorAPI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Mvc.Testing;
 using System.Net.Http.Json;
 using AcsEmulatorAPI.Models;
 
@@ -16,36 +9,48 @@ namespace AcsEmulatorAPI.Tests
     {
         private readonly WebApplicationFactory<Program> _factory;
         private readonly HttpClient _client;
-        private readonly string _authToken;
+        private string _authToken;
 
         public ChatControllerTests()
         {
             _factory = new WebApplicationFactory<Program>();
             _client = _factory.CreateClient();
+        }
+
+        [TestInitialize]
+        public async Task Initialize()
+        {
             // If Identity Unit tests is passing, below code will work
-            var identity = _client.PostAsJsonAsync("/identities/", new CreateIdentityRequest([IdentityTokenScope.Chat])).Result;
-            var token = _client.PostAsJsonAsync(identity.Headers.Location + "/:issueAccessToken", new IssueTokenRequest([IdentityTokenScope.Chat], 60)).Result;
-            var tokenResponse = token.Content.ReadFromJsonAsync<IdentityTokenResponse>().Result;
+            var identity = await _client.PostAsJsonAsync("/identities/", new CreateIdentityRequest([IdentityTokenScope.Chat]));
+            var token = await _client.PostAsJsonAsync(identity.Headers.Location + "/:issueAccessToken", new IssueTokenRequest([IdentityTokenScope.Chat], 60));
+            var tokenResponse = await token.Content.ReadFromJsonAsync<IdentityTokenResponse>();
             Assert.IsNotNull(tokenResponse);
-            _authToken = tokenResponse.token;    
+            _authToken = tokenResponse.token;
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            _client.Dispose();
         }
 
         [TestMethod()]
-        public void AddChatEndpointsTest()
+        public async Task AddChatEndpointsTest()
         {
             // Check if the /chat/threads endpoint is protected
-            var response = _client.PostAsJsonAsync("/chat/threads", new CreateChatThreadRequest("Test Topic", new List<ChatParticipant>())).Result;
+            var response = await _client.PostAsJsonAsync("/chat/threads", new CreateChatThreadRequest("Test Topic", new List<ChatParticipant>()));
             Assert.AreEqual(System.Net.HttpStatusCode.Unauthorized, response.StatusCode);
 
             // Add default Authorize header
             _client.DefaultRequestHeaders.Add("Authorization", "Bearer " + _authToken);
 
             // Create a New chat thread
-            response = _client.PostAsJsonAsync("/chat/threads", new CreateChatThreadRequest("Test Topic", new List<ChatParticipant>())).Result;
+            response = await _client.PostAsJsonAsync("/chat/threads", new CreateChatThreadRequest("Test Topic", new List<ChatParticipant>()));
             Assert.AreEqual(System.Net.HttpStatusCode.Created, response.StatusCode);
 
-            Console.WriteLine(response.Content.ReadAsStringAsync().Result);
-            var chatThreadCreation = response.Content.ReadFromJsonAsync<ChatThreadCreation>().Result;
+            string value = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(value);
+            var chatThreadCreation = await response.Content.ReadFromJsonAsync<ChatThreadCreation>();
             Assert.IsNotNull(chatThreadCreation);
             var chatThreadInfo = chatThreadCreation.ChatThread;
             // id must start with 19:
@@ -54,19 +59,19 @@ namespace AcsEmulatorAPI.Tests
 
 
             // Call Get /chat/threads
-            var chatThreads = _client.GetFromJsonAsync<ChatThreadInfo>("/chat/threads").Result;
+            var chatThreads = await _client.GetFromJsonAsync<ChatThreadInfo>("/chat/threads");
             Assert.IsNotNull(chatThreads);
             var firstChatThread = chatThreads.Value.First();
             Assert.AreEqual(chatThreadInfo.Id, firstChatThread.Id);
             Assert.AreEqual(chatThreadInfo.Topic, firstChatThread.Topic);
 
             // Call Get /chat/threads/{chatThreadInfo.Id}
-            var chatThread = _client.GetFromJsonAsync<ChatThreadCreationInfo>($"/chat/threads/{chatThreadInfo.Id}").Result;
+            var chatThread = await _client.GetFromJsonAsync<ChatThreadCreationInfo>($"/chat/threads/{chatThreadInfo.Id}");
             Assert.IsNotNull(chatThread);
             Assert.AreEqual(chatThreadInfo.Id, chatThread.Id);
 
             // Test the Get on admin chat endpoint
-            var adminChatThreads = _client.GetFromJsonAsync<ChatThreadInfo>("/admin/chat/threads").Result;
+            var adminChatThreads = await _client.GetFromJsonAsync<ChatThreadInfo>("/admin/chat/threads");
             Assert.IsNotNull(adminChatThreads);
             // Admin chat thread can show more info depending on DB, look for the specific Id
             Assert.IsTrue(adminChatThreads.Value.Any(x => x.Id == chatThreadInfo.Id));
