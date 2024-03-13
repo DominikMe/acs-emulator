b395d95d71211da265526539788865f20e7e14fb
Ralph Mari Gabrinao <rgabrinao@microsoft.com>
2024-03-12 22:58:18 -0700

    Add rest of properties. Save CallConnectionState as string so it is more legible in the db

--- AcsEmulator/AcsEmulatorAPI/CallAutomationController.cs
+++ AcsEmulator/AcsEmulatorAPI/CallAutomationController.cs
@@ -12,7 +12,7 @@ namespace AcsEmulatorAPI
             {
                 // MVP0: PhoneNumber places a call to a CommunicationUser
 
-                if (req.Targets.IsNullOrEmpty())
+                if (req.SourceCallerIdNumber == null || req.Targets.IsNullOrEmpty())
                 {
                     return Results.Forbid();
                 }
@@ -28,12 +28,15 @@ namespace AcsEmulatorAPI
                 {
                     CallConnectionProperties = new
                     {
-                        callConnection.Id,
+                        CallConnectionId = callConnection.Id,
+                        callConnection.AnsweredBy,
                         callConnection.CallConnectionState,
                         callConnection.CallbackUri,
                         callConnection.CorrelationId,
                         callConnection.ServerCallId,
+                        callConnection.Source,
                         callConnection.SourceCallerIdNumber,
+                        callConnection.SourceDisplayName,
                         Targets = callConnection.Targets.Select(x => new CommunicationIdentifier(x.RawId)).ToList()
                     }
                 };
--- AcsEmulator/AcsEmulatorAPI/Models/CallConnection.cs
+++ AcsEmulator/AcsEmulatorAPI/Models/CallConnection.cs
@@ -1,6 +1,7 @@
 ﻿#nullable disable
 
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AcsEmulatorAPI.Models
 {
@@ -9,8 +10,11 @@ namespace AcsEmulatorAPI.Models
         [Key]
         public Guid Id { get; set; }
 
+        public string AnsweredBy { get; set; }
+
         public CallConnectionState CallConnectionState { get; set; }
 
+        [Required]
         public string CallbackUri { get; set; }
 
         public string CognitiveServicesEndpoint { get; set; }
@@ -19,19 +23,21 @@ namespace AcsEmulatorAPI.Models
 
         public string ServerCallId { get; set; }
 
+        public string Source { get; set; }
+
         public string SourceCallerIdNumber { get; set; }
 
-        // TODO: Support PhoneNumber target
+        public string SourceDisplayName { get; set; }
+
+        [Required]
         public virtual ICollection<CallConnectionTarget> Targets { get; set; }
 
-        // TODO: Add these properties
-        //public CommunicationUser AnsweredBy { get; set; }
-
-        //public string Source { get; set; }
-
-        //public string SourceDisplayName { get; set; }
-
-        public static CallConnection CreateNew(string callbackUri, string sourceCallerIdNumber, string cognitiveServicesEndpoint = null)
+        public static CallConnection CreateNew(
+            string callbackUri,
+            string sourceCallerIdNumber,
+            string source = null,
+            string sourceCallerDisplayName = null,
+            string cognitiveServicesEndpoint = null)
         {
             return new CallConnection
             {
@@ -41,7 +47,9 @@ namespace AcsEmulatorAPI.Models
                 CognitiveServicesEndpoint = cognitiveServicesEndpoint,
                 CorrelationId = $"{Guid.NewGuid()}",
                 ServerCallId = $"{Guid.NewGuid()}",
+                Source = source,
                 SourceCallerIdNumber = sourceCallerIdNumber,
+                SourceDisplayName = sourceCallerDisplayName,
                 Targets = new List<CallConnectionTarget>()
             };
         }
@@ -52,6 +60,7 @@ namespace AcsEmulatorAPI.Models
             {
                 Id = Guid.NewGuid(),
                 RawId = x.CommunicationUser?.Id,
+                PhoneNumber = x.PhoneNumber?.Value,
                 CallConnection = this
             });
 
@@ -67,6 +76,8 @@ namespace AcsEmulatorAPI.Models
         public Guid Id { get; set; }
 
         public string RawId { get; set; }
+
+        public string PhoneNumber { get; set; }
 
         public virtual CallConnection CallConnection { get; set; }
     }
